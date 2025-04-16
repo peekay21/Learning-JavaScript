42532796763b846a179e79639ff732cf8c1af302
Pradip <pradip@Mac.mshome.net>
2025-04-16 16:54:05 +0530

    Adding a feature if age is less than 17 and greater than 4 then it will redirect to https://www.google.com

--- Chapter_6_Practice.js
+++ Chapter_6_Practice.js
@@ -1,11 +1,19 @@
 function canDrive(age){
     console.log(`Your age is ${age}`);
     if(age>=18){
+        a
         alert("You can drive!");
     }
-    else{
-        alert("You are not eligible to drive.");
+    else if(age>4){
+        location.href = 'https://www.google.com';
     }
+    else if (age <=0 ) {
+            console.error('Invalid negative input.');
+        }
+    else{
+        alert("You are not eligible to drive.");     
+    }
+    
 }
 
 do
